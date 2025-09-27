661a39b61c70a6b88141b13692294348c5f86586
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-09-27 17:21:01 +0600

    Adding cartDrawer with checkout Button

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -13,6 +13,20 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
           <IoMdClose className="h-6 w-6 text-gray-600 " />
         </button>
       </div>
+      {/* Cart contents With scrollable Area */}
+      <div className="flex-grow p-4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        {/* Cart items will be rendered here */}
+        {/* Component for cart Contents */}
+      </div>
+
+      {/* checkout Button fixxed at bottom*/}
+      <div className="p-4 bg-white sticky bottom-0">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+          CheckOut
+        </button>
+        <p>Shipping , taxes, and discount codes calculated at checkout</p>
+      </div>
     </div>
   );
 };
